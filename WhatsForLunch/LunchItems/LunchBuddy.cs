b1346acf4b35243c2768aaa6f89538cbdadacc08
Bob <bob.e.baxter@gmail.com>
2020-05-26 10:10:09 -0500

    first commit to store repo

---
+++ WhatsForLunch/LunchItems/LunchBuddy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhatsForLunch.LunchItems
+{
+    class LunchBuddy
+    {
+        public void Eat()
+        {
+
+        }
+
+        public void Eat(string food)
+        {
+
+        }
+
+        public void Eat(List<Employee> companions)
+        {
+
+        }
+
+        public void Eat(string food, List<Employee> companions)
+        {
+
+        }
+    }
+}
---
+++ WhatsForLunch/LunchItems/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhatsForLunch.LunchItems
+{
+    class Restaurant
+    {
+        List<string> Names = new List<string>();
+        Random rnd = new Random();
+
+        public Restaurant()
+        {
+            Names.Add("McDonald's");
+            Names.Add("Fat Moe's");
+            Names.Add("Five Guys");
+
+            string Name = Names[rnd.Next(Names.Count)];
+        }
+    }
+}
